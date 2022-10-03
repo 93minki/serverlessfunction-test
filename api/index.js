fdad53596a0267af04866bc420198c8b770d98c5
Minki Kim <alsrl0023@naver.com>
2022-10-03 11:55:18 +0900

    commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 export default async function handler(request, response) {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const res = await fetch(process.env.BASE_URL);
   const data = await res.json();
   return response.status(200).json({ data });
 }
--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
-      .then((result) => setData(result));
+    fetch("/api")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.data);
+        setLoaded(true);
+      });
   }, []);
 
   useEffect(() => {
     console.log(data);
   }, [data]);
 
-  return (
+  return loaded ? (
     <ol>
       {data.map(({ id, title }) => (
         <li key={id}>{title}</li>
       ))}
     </ol>
+  ) : (
+    "Data fetching..."
   );
 }
 
